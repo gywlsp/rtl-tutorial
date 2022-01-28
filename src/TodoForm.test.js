ca70f101c3f5837ef985c7965ab272d3ffb90593
gywlsp <gywls517@gmail.com>
2022-01-28 10:39:06 +0900

    test: :test_tube: TodoForm fail test 추가
    changes input

--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { screen, render } from "@testing-library/react";
+import { screen, render, fireEvent } from "@testing-library/react";
 
 import TodoForm from "./TodoForm";
 
@@ -8,5 +8,16 @@ describe("<TodoForm />", () => {
     render(<TodoForm />);
     screen.getByPlaceholderText("할 일을 입력하세요"); // input 이 있는지 확인
     screen.getByText("등록"); // button이 있는지 확인
+  });
+
+  it("changes input", () => {
+    render(<TodoForm />);
+    const input = screen.getByPlaceholderText("할 일을 입력하세요");
+    fireEvent.change(input, {
+      target: {
+        value: "TDD 배우기",
+      },
+    });
+    expect(input).toHaveAttribute("value", "TDD 배우기");
   });
 });
