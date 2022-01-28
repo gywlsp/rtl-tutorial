13d87c921dd634fa2d685e96cabd000235668829
gywlsp <gywls517@gmail.com>
2022-01-28 14:55:24 +0900

    feat: :sparkles: TodoApp 내 TodoList에 onRemove props 전달

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -44,10 +44,18 @@ const TodoApp = () => {
     },
     [todos]
   );
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos]
+  );
+
   return (
     <>
       <TodoForm onInsert={onInsert} />
-      <TodoList todos={todos} onToggle={onToggle} />
+      <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove} />
     </>
   );
 };
