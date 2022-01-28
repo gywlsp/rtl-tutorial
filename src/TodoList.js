2a376ebcde971f5d4ee518587180a2c8fc2908f3
gywlsp <gywls517@gmail.com>
2022-01-28 14:20:16 +0900

    feat: :sparkles: TodoList에 TodoItem 목록 추가

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,14 @@
 import React from "react";
+import TodoItem from "./TodoItem";
 
-const TodoList = () => {
-  return <ul />;
+const TodoList = ({ todos }) => {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem todo={todo} key={todo.id} />
+      ))}
+    </ul>
+  );
 };
 
 export default TodoList;
