600819fc53df86ee1970df2878c0d7c5409a7069
gywlsp <gywls517@gmail.com>
2022-01-28 10:07:09 +0900

    feat: :sparkles: Counter 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+
 import Profile from "./Profile";
+import Counter from "./Counter";
 
 const App = () => {
-  return <Profile username="gywlsp" name="박효진" />;
+  return (
+    <>
+      <Profile username="gywlsp" name="박효진" />
+      <Counter />
+    </>
+  );
 };
 
 export default App;
