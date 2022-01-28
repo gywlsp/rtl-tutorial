7733d274f269d2075551abbaee7fe21a244a834c
gywlsp <gywls517@gmail.com>
2022-01-28 14:47:06 +0900

    test: :test_tube: TodoApp fail test 추가
    creates new todo

--- src/TodoApp.test.js
+++ src/TodoApp.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 import TodoApp from "./TodoApp";
 
@@ -14,5 +14,18 @@ describe("<TodoApp />", () => {
     render(<TodoApp />);
     screen.getByText("TDD 배우기");
     screen.getByText("react-testing-library 사용하기");
+  });
+
+  it("creates new todo", () => {
+    render(<TodoApp />);
+    // 이벤트를 발생시켜서 새 항목을 추가하면
+    fireEvent.change(screen.getByPlaceholderText("할 일을 입력하세요"), {
+      target: {
+        value: "새 항목 추가하기",
+      },
+    });
+    fireEvent.click(screen.getByText("등록"));
+    // 해당 항목이 보여져야합니다.
+    screen.getByText("새 항목 추가하기");
   });
 });
