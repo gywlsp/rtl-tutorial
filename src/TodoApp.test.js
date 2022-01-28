600b2e9a813f538abcb59394856f8b65ccb8ecbb
gywlsp <gywls517@gmail.com>
2022-01-28 14:40:42 +0900

    test: :test_tube: TodoApp fail test 추가
    renders two default todos

--- src/TodoApp.test.js
+++ src/TodoApp.test.js
@@ -9,4 +9,10 @@ describe("<TodoApp />", () => {
     screen.getByText("등록"); // TodoForm 존재유무 확인
     screen.getByTestId("TodoList"); // TodoList 존재유무 확인
   });
+
+  it("renders two defaults todos", () => {
+    render(<TodoApp />);
+    screen.getByText("TDD 배우기");
+    screen.getByText("react-testing-library 사용하기");
+  });
 });
