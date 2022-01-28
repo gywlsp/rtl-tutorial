2f4bd6aace52526a7ff909572f93ac58c438b218
gywlsp <gywls517@gmail.com>
2022-01-28 10:45:13 +0900

    test: :test_tube: TodoForm fail test 추가
    calls on onInsert and clears input

--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -20,4 +20,21 @@ describe("<TodoForm />", () => {
     });
     expect(input).toHaveAttribute("value", "TDD 배우기");
   });
+
+  it("calls onInsert and clears input", () => {
+    const onInsert = jest.fn();
+    render(<TodoForm onInsert={onInsert} />);
+    const input = screen.getByPlaceholderText("할 일을 입력하세요");
+    const button = screen.getByText("등록");
+    // 수정하고
+    fireEvent.change(input, {
+      target: {
+        value: "TDD 배우기",
+      },
+    });
+    // 버튼 클릭
+    fireEvent.click(button);
+    expect(onInsert).toBeCalledWith("TDD 배우기"); // onInsert 가 'TDD 배우기' 파라미터가 호출됐어야함
+    expect(input).toHaveAttribute("value", ""); // input이 비워져야함
+  });
 });
